102084bc82680d8c30fa91f83e0cbba6f92f0f94
Christophe Barré <9792362+kreestof1@users.noreply.github.com>
2024-06-15 00:18:13 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,8 @@
-// create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-// create comments array
-const comments = [
-  { name: 'Stevie', comment: 'Wow!'},
-  { name: 'Linda', comment: 'Cool!'},
-  { name: 'Dan', comment: 'Nice!'}
-];
-// set view engine
-app.set('view engine', 'pug');
-// serve static files
-app.use(express.static('public'));
-// serve comments
-app.get('/comments', (req, res) => {
-  res.render('comments', { comments: comments });
-});
-// listen
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var comments = require('./comments.json');
+
